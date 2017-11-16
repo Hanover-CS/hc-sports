69b6be3880cdd1ca603f17a3305b3c6bb7d48180
Stephanie Iglesias <iglesiasst18@hanover.edu>
2017-11-16 17:10:23 -0500

    Initial Commit

---
+++ hc-sportsTests/hc_sportsTests.swift
@@ -0,0 +1,36 @@
+//
+//  hc_sportsTests.swift
+//  hc-sportsTests
+//
+//  Created by Stephanie Iglesias on 11/16/17.
+//  Copyright © 2017 Stephanie Iglesias. All rights reserved.
+//
+
+import XCTest
+@testable import hc_sports
+
+class hc_sportsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
