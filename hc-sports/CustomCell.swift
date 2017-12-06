6a2f669fb85a831614b43ee4778f3bf1ca04c5bd
Stephanie Iglesias <iglesiasst18@hanover.edu>
2017-12-06 03:54:13 -0500

    Started "Select Favorites" screen (for when user first downloads HC Sports App) and added it as a tab on the tab bar for right now --> need to research how to make this page appear once (when first downloaded) and store the data under a 'favorites list' to populate "Favorites" page

---
+++ hc-sports/CustomCell.swift
@@ -0,0 +1,15 @@
+//
+//  CustomCell.swift
+//  hc-sports
+//
+//  Created by Stephanie Iglesias on 12/6/17.
+//  Copyright © 2017 Stephanie Iglesias. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UICollectionViewCell {
+    
+    @IBOutlet weak var sportsImage: UIImageView!
+    
+}
