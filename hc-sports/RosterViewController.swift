42c45c1f9cc226179507b35512efb675fe7b7211
Stephanie Iglesias <iglesiasst18@hanover.edu>
2017-12-27 16:40:53 -0500

    Created simple Roster page per team -- need to fill in the UI for the roster page

---
+++ hc-sports/RosterViewController.swift
@@ -0,0 +1,43 @@
+//
+//  RosterViewController.swift
+//  hc-sports
+//
+//  Created by Stephanie Iglesias on 12/27/17.
+//  Copyright © 2017 Stephanie Iglesias. All rights reserved.
+//
+
+import UIKit
+
+class RosterViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        
+    }
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
