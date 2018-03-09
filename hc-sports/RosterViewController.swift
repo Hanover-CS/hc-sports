353ea98c731edd5317b6a26a66dad54fdc916d1c
Stephanie Iglesias <iglesiasst18@hanover.edu>
2018-03-09 00:28:43 -0500

    Getting closer to (successfully) populating my men's basketball roster page. Need to: 1. figure out how to list off all of the players, rather than one player for all table cells 2. replace the year, number, and position labels with actual data 3. add "Yr", "No.", and "Pos" texts in bold to the roster page's UI

--- hc-sports/RosterViewController.swift
+++ hc-sports/RosterViewController.swift
@@ -10,12 +10,12 @@ import UIKit
 
 class RosterViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    let images = [ "HC logo", "HC logo", "HC logo", "Hc logo", "Hc logo"]
-    let players = ["Leah", "Shannon", "Abby", "Cass", "Katie"]
+    var mBasketballPlayers = ["Cam Fails": ["Cam Fails", "Junior", "3", "Guard"], "Isaac Hibbard": ["Isaac Hibbard", "Freshman", "5", "Guard"], "Logan Ratts": ["Logan Ratts", "Sophomore", "11"], "Colin Smith": ["Colin Smith", "Senior", "12", "Guard"], "Levi Buck": ["Levi Buck", "Senior", "14", "Guard"], "John Williams": ["John Williams", "Sophomore", "15", "Guard"], "Jacob Bray": ["Jacob Bray", "Sophomore", "20", "Guard"], "Blake Robertson": ["Blake Robertson", "Sophomore", "21", "Guard"], "Jordan Armstrong": ["Jordan Armstrong", "Freshman", "22", "Forward"], "Ryan Cloncs": ["Ryan Cloncs", "Sophomore", "25", "Forward"], "Christain Reed": ["Christain Reed", "Sophomore", "32", "Forward"], "Jackson Crawford": ["Jackson Crawford", "Sophomore", "33", "Forward"], "Wes McKinney": ["Wes McKinney", "Senior", "35", "Forward"], "Kevin Williams": ["Kevin Williams", "Sophomore", "40", "Forward"], "Nick White": ["Nick White", "Junior", "42", "Forward"], "Connor Washburn": ["Connor Washburn", "Freshman", "45", "Center"], "Parker Buse": ["Parker Buse", "Senior", "52", "Center"]]
+    
     
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
-        return images.count
+        return mBasketballPlayers.count
     }
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
@@ -23,6 +23,12 @@ class RosterViewController: UIViewController, UITableViewDataSource, UITableView
         
 //        cell.playerPhoto.image = UIImage(named: (images[indexPath.row] + ".jpg"))
 //        cell.playerName.text = players[indexPath.row]
+//        cell.playerPhoto.image = Array(self.players.values)[indexPath.row]
+        
+        for (key, value) in mBasketballPlayers {
+            cell.playerName.text = key
+            cell.playerPhoto.image = UIImage(named: value[0])
+        }
         
         return cell
     }
