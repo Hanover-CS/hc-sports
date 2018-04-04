b7207caca630f7370fae19278c37d9a48e835f65
Stephanie Iglesias <stephanieiglesias@stephanies-macbook-pro.local>
2018-04-04 14:09:41 -0400

    Created folders for each tab.

--- hc-sports/MyTableViewCell.swift
+++ hc-sports/MyTableViewCell.swift
@@ -8,19 +8,20 @@
 
 import UIKit
 
-class MyTableViewCell: UITableViewCell {
-    // Prepares the receiver for service after it has been loaded from an Interface Builder archive, or nib file.
-    override func awakeFromNib() {
-        super.awakeFromNib()
-    }
+//class MyTableViewCell: UITableViewCell {
+//    // Prepares the receiver for service after it has been loaded from an Interface Builder archive, or nib file.
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//    }
+//
+//    /*:
+//        Sets the selected state of the cell, optionally animating the transition between states.
+//        - parameter:
+//            selected: true to set the cell as selected, false to set it as unselected. The default is false.
+//            animated: true to animate the transition between selected states, false to make the transition immediate.
+//     */
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//    }
+//}
 
-    /*:
-        Sets the selected state of the cell, optionally animating the transition between states.
-        - parameter:
-            selected: true to set the cell as selected, false to set it as unselected. The default is false.
-            animated: true to animate the transition between selected states, false to make the transition immediate.
-     */
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-    }
-}
